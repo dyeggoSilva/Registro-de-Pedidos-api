91abdb923c42eb799fb3f1e90b45d4de56d4e3b0
Dyeggo Silva <dyeggo0102@live.com>
2023-12-10 12:34:59 -0300

    api-registro-de-pedido
    Projeto completo, CRUD feito em NodeJS, Express e SQLite

---
+++ app.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Adicione esta linha
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para permitir o uso de JSON no corpo das requisições
+app.use(bodyParser.json());
+app.use(cors()); // Adicione esta linha para habilitar o CORS
+
+// Conectar ao banco de dados SQLite (cria o banco se não existir)
+const db = new sqlite3.Database('./mydatabase.db');
+
+// Criação da tabela "pedidos" se não existir
+db.run(`CREATE TABLE IF NOT EXISTS pedidos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pedido TEXT,
+    nome TEXT,
+    produto TEXT,
+    telefone TEXT
+)`);
+
+// Endpoint para listar todos os pedidos
+app.get('/pedidos', (req, res) => {
+    db.all('SELECT * FROM pedidos', (err, rows) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Erro ao buscar pedidos' });
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+// Endpoint para criar um novo pedido
+app.post('/pedidos', (req, res) => {
+    const { pedido, nome, produto, telefone } = req.body;
+    db.run(
+        'INSERT INTO pedidos (pedido, nome, produto, telefone) VALUES (?, ?, ?, ?)',
+        [pedido, nome, produto, telefone],
+        function (err) {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Erro ao criar pedido' });
+            } else {
+                res.json({ id: this.lastID, pedido, nome, produto, telefone });
+            }
+        }
+    );
+});
+
+// Rota para atualizar um dado existente
+app.put('/dados/:id', (req, res) => {
+    const id = req.params.id;
+    const { pedido, nome, produto, telefone } = req.body;
+  
+    db.run('UPDATE pedidos SET pedido=?, nome=?, produto=?, telefone=? WHERE id=?', [pedido, nome, produto, telefone, id], function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({ mensagem: 'Dado atualizado com sucesso.' });
+    });
+  });
+  
+  // Rota para excluir um dado
+  app.delete('/dados/:id', (req, res) => {
+    const id = req.params.id;
+  
+    db.run('DELETE FROM pedidos WHERE id=?', id, function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({ mensagem: 'Dado excluído com sucesso.' });
+    });
+  });
+
+  
+  app.patch('/dados/:id', (req, res) => {
+    const id = req.params.id;
+    const { pedido, nome, produto, telefone } = req.body;
+  
+    db.run('UPDATE pedidos SET pedido=?, nome=?, produto=?, telefone=? WHERE id=?', [pedido, nome, produto, telefone, id], function (err) {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({ mensagem: 'Dado atualizado com sucesso.' });
+    });
+  });
+
+// Inicie o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
